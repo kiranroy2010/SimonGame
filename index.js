4f88518f7e835ece6db9affb67b5d93b2d146d25
Kiranroy <67857778+kiranroy2010@users.noreply.github.com>
2022-04-15 22:51:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ $(document).keypress(function() {
   }
 });
 
+$(document).click(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
 $(".btn").click(function() {
 
   var userChosenColour = $(this).attr("id");
